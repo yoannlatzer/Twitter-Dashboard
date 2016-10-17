f249c5dec84a2a6bd5356b7a2861431e6a22190c
Luuk Lenders <luuklsll@gmail.com>
2016-10-17 22:38:18 +0200

    ads.js
    Base idea on how ads.js should work. Functions not included.

---
+++ template_static/ads.js
@@ -0,0 +1,17 @@
+(function($, block) {
+block.fn.ads = function(config) {
+
+//functie voor gem (avg(sum(ta uit heatmap.temperatures)))
+
+//neem je regen mee in bepaling??
+
+//return op wiskundige basis van gem en regen per dag een ad + hyperlink in html format
+//prob returns getal? getal = img + link
+
+ // <a href="getal_link">  <img src="getal_img"  alt="getal_alt">
+
+
+
+    return this.$element;
+};
+})(jQuery, block);
