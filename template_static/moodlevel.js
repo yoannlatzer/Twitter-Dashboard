2ee9b6b660ec0267bf2fa83b1cd37324af037fa0
yoannlatzer <yoannlatzer@gmail.com>
2016-10-20 15:04:49 +0200

    Mood Meter Update: Rotated it and added a mood text indication

--- template_static/moodlevel.js
+++ template_static/moodlevel.js
@@ -9,15 +9,19 @@
     $('#mood-div').animate({"height": mapInt(moodLevel, 0, 2, 0, 250) + "px"});
 
     if (moodLevel < 1) {
+      $('#moodtxt').remove();
       $('#mood-div').animate({backgroundColor: "#F44336"});
-      //$('#mood-container').append("<p>Happy</p>");
+      $('#mood-container').append('<p id="moodtxt">Current Mood: Sad</p>');
     }
     else if (moodLevel > 1) {
+      $('#moodtxt').remove();
       $('#mood-div').animate({backgroundColor: "#00E676"});
-      //$('#mood-container').append("<p>Sad</p>");
+      $('#mood-container').append('<p id="moodtxt">Current Mood: Happy</p>');
     }
     else {
+      $('#moodtxt').remove();
       $('#mood-div').animate({backgroundColor: "#FFF176"});
+      $('#mood-container').append('<p id="moodtxt">Current Mood: Average</p>');
     }
   }
 
