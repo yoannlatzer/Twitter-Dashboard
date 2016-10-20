0025d860e6f3f93d3e16dc192f0307e70f0474ed
Sander Mandemaker <sander@corbifex.com>
2016-10-20 19:31:24 +0200

    Comments date.js

--- template_static/date.js
+++ template_static/date.js
@@ -1,10 +1,20 @@
+/**
+ * Date shows the current date
+ */
 (function($, block) {
 block.fn.date = function(config) {
+    // set date cache variable
+    var date = 0
     this.$element.append('<p>');
-
     this.actions(function(e, message){
+      // only update date if message.date != date (cache)
+      if (message.date != date ) {
+        // set new cache date
+        date = message.date
+        // make date html element
         $p = $('p', this);
         $p.html('<b>Current date: ' + message.date.substr(-2) + '-' + message.date.substr(-4, 2) + '-' + message.date.substr(0, 4) + '</b>');
+      }
     });
 
     return this.$element;
