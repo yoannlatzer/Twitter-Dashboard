2cbe960aa21f221b27aa5892e7585d520d440904
keving119 <kgeevers@live.nl>
2016-10-21 22:30:09 +0200

    Restyle dashboard

--- template_static/date.js
+++ template_static/date.js
@@ -5,15 +5,15 @@
   block.fn.date = function(config) {
     // set date cache variable
     var date = 0;
-    this.$element.append('<p>');
+    this.$element.append('<b>');
     this.actions(function(e, message){
       // only update date if message.date != date (cache)
       if (message.date != date ) {
         // set new cache date
         date = message.date;
         // make date html element
-        $p = $('p', this);
-        $p.html('<b>Current date: ' + message.date.substr(-2) + '-' + message.date.substr(-4, 2) + '-' + message.date.substr(0, 4) + '</b>');
+        $p = $('b', this);
+        $p.html('Current date: ' + message.date.substr(-2) + '-' + message.date.substr(-4, 2) + '-' + message.date.substr(0, 4) + '');
       }
     });
 
